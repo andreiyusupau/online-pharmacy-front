dcb0264d2642c2d945b32c7cd5ab346b15d9c4d9
andreiyusupau <nevermind.qqq@gmail.com>
2020-11-16 22:25:04 +0300

    Add async await.

--- src/services/auth.service.tsx
+++ src/services/auth.service.tsx
@@ -2,31 +2,35 @@ import axios from "axios";
 
 const API_URL = "http://localhost:8080/authentication";
 
-export function register(firstName:string,middleName:string,lastName:string,dateOfBirth:string, email:string, password:string,confirmPassword:string) {
-    return axios.post(API_URL + "/register", {
-        firstName,
-        middleName,
-        lastName,
-        dateOfBirth,
-        email,
-        password,
-        confirmPassword
-    })
-        .then((response) =>  response.data,
-            (error) => console.log(error));
-}
-
-export function login (email:string, password:string){
-    return axios
-        .post(API_URL + "/login", {
+export async function register(firstName: string, middleName: string, lastName: string, dateOfBirth: string, email: string, password: string, confirmPassword: string) {
+    try {
+        const response = await axios.post(API_URL + "/register", {
+            firstName,
+            middleName,
+            lastName,
+            dateOfBirth,
             email,
             password,
+            confirmPassword
         })
-        .then((response) => {
-            if (response.data.accessToken) {
-                localStorage.setItem("user", JSON.stringify(response.data));
-            }
-            return response.data;
+        return response.data;
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export async function login(email: string, password: string) {
+    try {
+        const response = await axios.post(API_URL + "/login", {
+            email,
+            password,
         });
+        if (response.data.accessToken) {
+            localStorage.setItem("user", JSON.stringify(response.data));
+        }
+        return response.data;
+    } catch (e) {
+        console.log(e);
+    }
 }
 
